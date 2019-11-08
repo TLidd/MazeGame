fabf1d019394ebee0cd5afe7784b4cce67b9592f
Jessie Dulay Aniguid <janiguid@crm.ucsc.edu>
2019-11-07 21:13:47 -0800

    Camera Rotates

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform playerBody;
+
+    public float sensitivity = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xRot = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float yRot = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        //float finalYRot = Mathf.Clamp(yRot, -90, 90);
+
+        transform.Rotate(-Vector3.right * Mathf.Clamp(yRot, -90, 90));
+        //transform.localRotation = Quaternion.Euler(finalYRot, 0, 0);
+        //X movement
+        playerBody.Rotate(Vector3.up * xRot);
+    }
+}
