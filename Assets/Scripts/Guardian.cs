6cc920b89e94c5c6e60a768a864cbfa329cafdf2
Andrew Purcell <apurcell910@gmail.com>
2019-11-14 02:13:17 -0800

    Added keypad ability for obstacles

--- Assets/Scripts/Guardian.cs
+++ Assets/Scripts/Guardian.cs
@@ -14,11 +14,11 @@ public class Guardian : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) {
             mode = 1;
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+        } else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)) {
             mode = 2;
-        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+        } else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)) {
             mode = 3;
         }
     }
